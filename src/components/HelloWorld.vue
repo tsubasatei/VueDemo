daa7ed6173b89351b001ffae9a02c81e0b032dfc
tsubasatei <tsubasa310@163.com>
2018-12-27 11:01:43 +0800

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <p class="msg">{{msg}}</p>
+  </div>
+</template>
+
+<script>
+  export default { // 配置对象(与vue一致)
+    data () { // 组件里必须写函数
+      return {
+        msg: 'Hello Vue Component'
+      }
+    }
+  }
+</script>
+
+<style>
+  .msg {
+    color: red;
+    fontSize: 30px
+  }
+</style>
